08663f9b3191e18855c5d632c58a2ecb60249158
Kapil Sharma <kapils0298@gmail.com>
2022-10-04 19:44:37 +0530

    Practice code and correction

--- Front-end/Learning/JS/script.js
+++ Front-end/Learning/JS/script.js
@@ -1,15 +1,37 @@
 // alert('Hello');
 // document.write('Hello World');//for testing purpose
-console.log(10);//debugging code
+// console.log(10);//debugging code
 
 
 // variables
 var num1;//declaration
 num1 = 20; //initialization
-
 var num2 = 30;
+
 var firstName = 'Kapil';
 var lastName = 'Sharma';
 
+var age = '25';
+var city = 'Delhi';
+
+var designation = 'Software Engineer';
+var salary = '60000';
+
+// printing on console
 console.log(num1 + num2);
 console.log(firstName, lastName);
+console.log('Age is', age);
+console.log('City is', city);
+console.log('Designation is', designation);
+console.log('Salary is', salary);
+
+//printing om UI
+document.write(firstName, lastName);
+document.write(age);
+document.write(city);
+document.write(designation);
+document.write(salary);
+
+
+
+// 👆 Code for practicing only
