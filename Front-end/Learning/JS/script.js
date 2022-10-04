08b7acc86696898a047d0dc384a54c66880778cf
Kapil Sharma <kapils0298@gmail.com>
2022-10-04 08:45:02 +0530

    JavaScripts intro and variables

---
+++ Front-end/Learning/JS/script.js
@@ -0,0 +1,15 @@
+// alert('Hello');
+// document.write('Hello World');//for testing purpose
+console.log(10);//debugging code
+
+
+// variables
+var num1;//declaration
+num1 = 20; //initialization
+
+var num2 = 30;
+var firstName = 'Kapil';
+var lastName = 'Sharma';
+
+console.log(num1 + num2);
+console.log(firstName, lastName);
